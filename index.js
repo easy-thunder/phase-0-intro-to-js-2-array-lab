e3c46bdbac089dd6d4bf299989cfb91c44649a2c
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-13 08:03:21 -0700

    commit tree f60d827d3bbc49f21db5813013aca066658dff0e

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Write your solution here!
+'use strict'
+
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(){cats.push("Ralph")};
+
+function destructivelyPrependCat(){cats.unshift("Bob")};
+
+function destructivelyRemoveLastCat(){cats.pop()};
+
+function destructivelyRemoveFirstCat(){cats.shift()};
+
+
+function appendCat(){
+    const appendedCats=[...cats,"Broom"];
+    return appendedCats;
+};
+
+function prependCat(){
+    const prependedCats = ["Arnold", ...cats];
+    return prependedCats;
+};
+
+function removeLastCat(){
+    const removeLastCat =cats.slice(0,2);
+    return removeLastCat;
+};
+
+console.log(removeLastCat());
+
+
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1,3);
+    return removeFirstCat;
+};
+
+console.log(removeFirstCat());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
